e31a4db673b18360393e7bc7ada47f1d54d3c920
Jobayer Hosen <juba2732@gmail.com>
2021-10-29 16:16:28 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("hello from backend");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.judr2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connected to db");
+    const db = client.db("go-trip");
+    const eventCollection = db.collection("events");
+
+    app.get("/events", async (req, res) => {
+      const cursor = eventCollection.find({});
+      const events = await cursor.toArray();
+      console.log(events);
+      if (events) {
+        res.json(events);
+      } else {
+        res.status(404).send("No Events Found");
+      }
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("Listening to port: ", port);
+});
