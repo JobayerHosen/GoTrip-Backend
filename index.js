38d408f6e18caeb033fea6e8509ac39a99b4194a
Jobayer Hosen <juba2732@gmail.com>
2021-10-30 02:15:43 +0600

    added 'get single event api' and 'add user api'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ObjectId = require("mongodb").ObjectId;
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
@@ -23,7 +24,9 @@ async function run() {
     console.log("connected to db");
     const db = client.db("go-trip");
     const eventCollection = db.collection("events");
+    const userCollection = db.collection("users");
 
+    //GET ALL EVENTS API
     app.get("/events", async (req, res) => {
       const cursor = eventCollection.find({});
       const events = await cursor.toArray();
@@ -34,6 +37,37 @@ async function run() {
         res.status(404).send("No Events Found");
       }
     });
+
+    // GET SINGLE EVENT API
+    app.get("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const event = await eventCollection.findOne({ _id: ObjectId(id) });
+
+      if (event._id == id) {
+        res.json(event);
+      } else {
+        res.status(404).send("No Event Found");
+      }
+    });
+
+    // ADD USER API
+    app.post("/users/addUser", async (req, res) => {
+      const user = req.body;
+
+      if (!user.email) {
+        res.status(403).send("Invalid Input");
+      }
+
+      const result = await userCollection.insertOne(user);
+      console.log(result, user);
+      if (result.acknowledged) {
+        res.json(user);
+      } else {
+        res.status(500).send("Internal server error");
+      }
+    });
+  } catch (err) {
+    console.log(err);
   } finally {
     // await client.close();
   }
