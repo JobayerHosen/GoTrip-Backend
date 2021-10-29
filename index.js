e7f614446ed13e393619c21477a1787947dd74eb
Jobayer Hosen <juba2732@gmail.com>
2021-10-29 19:30:35 +0600

    fix port

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json("hello from backend");
+  res.send("api is up");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.judr2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
