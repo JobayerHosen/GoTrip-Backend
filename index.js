16d651a5ac5c727d46e70af5a57bf4535e3b81f7
Jobayer Hosen <juba2732@gmail.com>
2021-10-30 19:08:37 +0600

    add api for 'add user', 'get single user', 'update user' , place order

--- index.js
+++ index.js
@@ -20,52 +20,131 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
   try {
+    //DB CONNECTION--------------------------------
     await client.connect();
     console.log("connected to db");
     const db = client.db("go-trip");
     const eventCollection = db.collection("events");
     const userCollection = db.collection("users");
+    const orderCollection = db.collection("orders");
 
+    //---------APIs------------------------------------------------------------------------------
     //GET ALL EVENTS API
     app.get("/events", async (req, res) => {
-      const cursor = eventCollection.find({});
-      const events = await cursor.toArray();
-      console.log(events);
-      if (events) {
-        res.json(events);
-      } else {
-        res.status(404).send("No Events Found");
+      try {
+        const cursor = eventCollection.find({});
+        const events = await cursor.toArray();
+        if (events) {
+          res.json(events);
+        } else {
+          res.status(404).send("No Events Found");
+        }
+      } catch (err) {
+        res.status(500).send("interna server error: ", err);
       }
     });
 
     // GET SINGLE EVENT API
     app.get("/events/:id", async (req, res) => {
-      const id = req.params.id;
-      const event = await eventCollection.findOne({ _id: ObjectId(id) });
+      try {
+        const id = req.params.id;
+        const event = await eventCollection.findOne({ _id: ObjectId(id) });
 
-      if (event._id == id) {
-        res.json(event);
-      } else {
-        res.status(404).send("No Event Found");
+        if (event._id == id) {
+          res.json(event);
+        } else {
+          res.status(404).send("No Event Found");
+        }
+      } catch (err) {
+        res.status(500).send("interna server error: ", err);
+      }
+    });
+
+    // GET SINGLE USER API
+    app.get("/users/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const user = await userCollection.findOne({ uid: id });
+
+        if (user.uid == id) {
+          res.json(user);
+        } else {
+          res.status(404).send("No Event Found");
+        }
+      } catch (err) {
+        res.status(500).send("interna server error: ", err);
       }
     });
 
     // ADD USER API
     app.post("/users/addUser", async (req, res) => {
-      const user = req.body;
+      try {
+        const user = req.body;
 
-      if (!user.email) {
-        res.status(403).send("Invalid Input");
-      }
+        if (!user.email) {
+          res.status(403).send("Invalid Input");
+        }
 
-      const result = await userCollection.insertOne(user);
-      console.log(result, user);
-      if (result.acknowledged) {
-        res.json(user);
-      } else {
-        res.status(500).send("Internal server error");
+        const result = await userCollection.insertOne(user);
+        if (result.acknowledged) {
+          res.json(user);
+        } else {
+          throw new Error("Could not add User");
+        }
+      } catch (err) {
+        res.status(500).send("interna server error: ", err);
       }
     });
+
+    // UPDATE USER API
+    app.put("/users/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const user = req.body;
+
+        const filter = await userCollection.findOne({ uid: id });
+        const options = { upsert: true };
+
+        const updateDoc = {
+          $set: {
+            name: user.name,
+            phone: user.phone,
+            address: user.address,
+          },
+        };
+
+        if (user.uid === filter.uid) {
+          const result = await userCollection.updateOne(filter, updateDoc, options);
+
+          res.json(user);
+        } else {
+          res.status(404).send("could not updated");
+        }
+      } catch (err) {
+        res.status(500).send(`interna server error: ${err}`);
+      }
+    });
+
+    // ADD ORDER/BOOKING API
+    app.post("/orders/addOrder", async (req, res) => {
+      try {
+        const order = req.body;
+        if (!order.uid) {
+          res.status(403).send("Invalid Input");
+        }
+
+        const result = await orderCollection.insertOne(order);
+        if (result.acknowledged) {
+          res.json(order);
+        } else {
+          res.status(500).send("Internal Server Error");
+        }
+      } catch (err) {
+        res.status(500).send(`interna server error: ${err}`);
+      }
+    });
+
+    //--------------------------------------------------------------------------
   } catch (err) {
     console.log(err);
   } finally {
